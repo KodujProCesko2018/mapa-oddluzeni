781b1f126e547053a7b14de289fe8f2ff896540d
Jakub Žaba <jakub.zaba@kiwi.com>
2018-05-26 09:57:08 +0200

    Add Redux for server

--- web/pages/index.tsx
+++ web/pages/index.tsx
@@ -1,8 +1,17 @@
 import React from 'react'
 import Link from 'next/link'
+import Layout from '../src/components/Layout';
 
-export default () => (
-  <ul>
-    <li><Link href='/about' as='/o-projektu'><a>O projektu</a></Link></li>
-  </ul>
+const Index = () => (
+    <Layout>
+        <div>
+            <ul>
+                <li>
+                    <Link href='/about' as='/o-projektu'><a>O projektu</a></Link>
+                </li>
+            </ul>
+        </div>
+    </Layout>
 )
+
+export default Index
