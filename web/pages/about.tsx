f34e7006c0bf330ebe3f149684c193c8b2f74a55
Jakub Žaba <jakub.zaba@kiwi.com>
2018-05-26 08:54:27 +0200

    Add initial web server

---
+++ web/pages/about.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+
+export default () => (
+    <div>
+        <a href='/'>Zpět</a>
+        <h1>O projektu</h1>
+    </div>
+);
---
+++ web/pages/index.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import Link from 'next/link'
+
+export default () => (
+  <ul>
+    <li><Link href='/about' as='/o-projektu'><a>O projektu</a></Link></li>
+  </ul>
+)
