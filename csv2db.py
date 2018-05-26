343bf02998b942c1698b1fa0c2da1625de06fd02
Vít Baisa <vit.baisa@sketchengine.co.uk>
2018-05-27 01:15:36 +0200

    process rejstrik into a simple db

--- csv2db.py
+++ csv2db.py
@@ -4,17 +4,51 @@
 import sys
 import sqlite3
 
+meta = ["idOsobyPuvodce", "druhStavRizeni", "nazevOsoby", "idOsoby",
+        "druhRoleVRizeni", "druhOsoby", "jmeno", "rc", "datumPobytOd",
+        "adresa", "psc", "mesto", "idAdresy", "cisloPopisne", "ulice",
+        "datumNarozeni", "ic", "druhPravniForma", "titulPred", "okres", "zeme",
+        "druhSpravce", "nazevOsobyObchodni", "priznakAnInterniCisUdalost",
+        "popisCisUdalosti", "datumZalozeniCisUdalosti", "cisUdalost",
+        "telefon", "fax", "datumZruseniCisUdalosti"]
+
+ins_list = {}
+
+def parse_details(str):
+    x = str.replace('ns2:', '').replace('udalost:', '').replace('vec:', '').replace('osoba:', '')
+    d = dict([(x.split(':', 1)[0], x.split(':', 1)[1]) for x in x.split(';;') if x.strip()])
+    o = {}
+    for m in meta:
+        v = d.get(m, '')
+        if v:
+            o[m] = v
+    return o
+
 for line in sys.stdin:
     try:
-        i, kod, od, do, ins, _, event, details = line.split('\t')
+        i, kod, _, publish, ins, _, event, details = line.split('\t')
     except ValueError:
         continue
-    if int(kod) in [174, 175]:
-        details = details.replace('ns2:', '').replace('udalost:', '').replace('vec:', '')
-        #print 'DETAILS:', details
-        d = {}
-        for x in details.split(';;'):
-            if x.strip():
-                a, b = x.split(':', 1)
-                d[a] = b
-        print kod, d.get('nazevOsoby', 'NAZEV_OSOBY'), d.get('idOsobyPuvodce', 'SOUDXXX')
+    publish = publish[:10]
+    kod = int(kod)
+    ins = ins[4:]
+    if kod in [174, 175, 177, 178]:
+        # pocatek insolvence
+        if kod in [174, 175]:
+            # store the last event with this code
+            ins_list[ins] = {
+                'ins_start': publish,
+                'kod_start': kod
+            }
+        # konec insolvence
+        if kod in [177, 178]:
+            if ins in ins_list:
+                ins_list[ins]['ins_end'] = publish
+                ins_list[ins]['kod_end'] = kod
+
+for ins in ins_list:
+    print "INSERT INTO insolvence (ins, date1, code1, date2, code2) VALUES ('%s', '%s', %d, '%s', %d);" %\
+            (ins, ins_list[ins].get('ins_start', '????-??-??'),
+            ins_list[ins].get('kod_start', -1),
+            ins_list[ins].get('ins_end', '????-??-??'),
+            ins_list[ins].get('kod_end', -1))
